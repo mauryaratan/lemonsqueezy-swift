0805a561a24b186e9e4afcb1394ece4b8f03bdef
Ram Ratan Maurya <ratan.maurya@gmail.com>
2023-02-05 18:53:33 +0530

    Named parameteres for licensing api

--- Sources/LemonSqueezy/LemonSqueezy+License.swift
+++ Sources/LemonSqueezy/LemonSqueezy+License.swift
@@ -8,15 +8,33 @@
 import Foundation
 
 extension LemonSqueezy {
-    public func activateLicense(queryItems: [URLQueryItem]) async throws -> ActivateLicense {
+    public func activateLicense(licenseKey: String, instanceName: String) async throws -> ActivateLicense {
+        let queryItems: [URLQueryItem] = [
+            URLQueryItem(name: "license_key", value: licenseKey),
+            URLQueryItem(name: "instance_name", value: instanceName)
+        ]
+        
         return try await call(route: .activateLicense, method: .POST, queryItems: queryItems)
     }
     
-    public func validateLicense(queryItems: [URLQueryItem]) async throws -> ValidateLicense {
+    public func validateLicense(licenseKey: String, instanceId: String?) async throws -> ValidateLicense {
+        var queryItems: [URLQueryItem] = []
+        
+        queryItems.append(URLQueryItem(name: "license_key", value: licenseKey))
+        
+        if instanceId != nil {
+            queryItems.append(URLQueryItem(name: "instance_id", value: instanceId))
+        }
+        
         return try await call(route: .validateLicense, method: .POST, queryItems: queryItems)
     }
     
-    public func deactivateLicense(queryItems: [URLQueryItem]) async throws -> DeactivateLicense {
+    public func deactivateLicense(licenseKey: String, instanceId: String) async throws -> DeactivateLicense {
+        let queryItems: [URLQueryItem] = [
+            URLQueryItem(name: "license_key", value: licenseKey),
+            URLQueryItem(name: "instance_id", value: instanceId)
+        ]
+        
         return try await call(route: .deactivateLicense, method: .POST, queryItems: queryItems)
     }
 }
